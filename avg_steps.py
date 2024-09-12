1d822a1b58e7d5c11372907adca4f0bb7059dfd4
Asitha Mudiyanselage <asitha_mudiyanselage@baylor.edu>
2024-09-11 23:15:13 -0500

    Adjusted the problem 3. Not done yet

---
+++ avg_steps.py
@@ -0,0 +1,52 @@
+import csv
+
+steps = open('steps.csv', 'r')
+
+st_obj = csv.reader(steps)
+
+next(st_obj)
+
+my_dict = {'1' : 'January',
+          '2' : 'February',
+          '3' : 'March',
+          '4' : 'April',
+          '5' : 'May',
+          '6' : 'June',
+          '7' : 'July',
+          '8' : 'Augest',
+          '9' : 'September',
+          '10': 'Octomber',
+          '11': 'November',          
+          '12': 'December'}
+
+my_list = ['1','2','3','4','5','6','7','8','9','10','11','12']
+
+counter = 1
+Total   = 0
+
+for rec in my_list:
+
+    for line in st_obj:
+
+        if line[0] == rec:
+        
+            Month = my_dict[rec]   
+
+            counter += 1
+
+            Total   += int(line[1])
+
+            avg_step = Total/counter
+
+        else:
+
+            break
+            
+    print( f"{Month}"+'- ' + str(f"{avg_step: ,.2f}") )
+
+
+
+
+
+
+  
