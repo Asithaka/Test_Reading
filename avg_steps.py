764156d5e4e234a368169d335e93614a401353c7
Asitha Mudiyanselage <asitha_mudiyanselage@baylor.edu>
2024-09-16 15:37:11 -0500

    Finished question 3 without  using dictionary

--- avg_steps.py
+++ avg_steps.py
@@ -6,45 +6,48 @@ st_obj = csv.reader(steps)
 
 next(st_obj)
 
-my_dict = {'1' : 'January',
-          '2' : 'February',
-          '3' : 'March',
-          '4' : 'April',
-          '5' : 'May',
-          '6' : 'June',
-          '7' : 'July',
-          '8' : 'Augest',
-          '9' : 'September',
-          '10': 'Octomber',
-          '11': 'November',          
-          '12': 'December'}
+rows = list(st_obj)
+
+# my_dict = {'1' : 'January',
+#           '2' : 'February',
+#           '3' : 'March',
+#           '4' : 'April',
+#           '5' : 'May',
+#           '6' : 'June',
+#           '7' : 'July',
+#           '8' : 'Augest',
+#           '9' : 'September',
+#           '10': 'Octomber',
+#           '11': 'November',          
+#           '12': 'December'}
 
 my_list = ['1','2','3','4','5','6','7','8','9','10','11','12']
 
+my_month = ['January','February','March','April','May','June','July','Augest','September','Octomber','November','December']
+
 for rec in my_list:
 
-    counter = 1
+    counter = 0
     total   = 0
   
-    for line in st_obj: 
-                
-            if  line[0] == rec:
-
-                Month = my_dict[rec]   
+    for line in rows: 
+                   
+        if  line[0] == rec:   
 
                 counter += 1
 
-                total   +=  int(line[1])
-            
-                avg_step = total/counter
+                total   +=  float(line[1])
+        Month = my_month[int(rec)-1]         
+    avg_step = total/counter
 
-            else:
 
-                break    
+            #else:
+
+            #    break    
               
     #print( f"{Month}"+'- ' + str(f"{avg_step: ,.2f}, {counter},{total}") )
 
-print( f"{Month}"+'- ' + str(f"{avg_step: ,.2f}") )
+    print( f"{Month}"+'- ' + str(f"{avg_step: ,.2f}") )
 
 
 
